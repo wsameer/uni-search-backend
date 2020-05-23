66690858f57af96aac7a418fdfcd20e29d8ec1df
nevrmore <dev.wsameer@gmail.com>
2020-05-23 22:50:43 +0530

    removed unused code

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 // load up the express framework
 const express = require('express');
 
-// load the morgan lib for logging - dev
-const log = require('morgan')('dev');
-
 // load the properties config file
 const properties = require('./config/properties');
 
@@ -22,19 +19,15 @@ const app = express();
 // initialise express router
 const router = express.Router();
 
-// configure logger
-app.use(log);
-
 // configure our express instance with some body-parser settings 
 // including handling JSON data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Error handling
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization");
   next();
 });
